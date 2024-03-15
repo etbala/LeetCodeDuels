1849a71cf7cdda8ee92a1c33436d0a3fbedcdb1c
EdenBendheim <edenbendheim@gmail.com>
2024-03-15 16:51:46 -0400

    joe mami

--- extensions/scripts/background.js
+++ extensions/scripts/background.js
@@ -1,9 +1,19 @@
-try {
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-        if (changeInfo.status === 'complete') {
-            chrome.tabs.executeScript(tabId, {file: 'test.js'});
-        }
-    });
-} catch (e) {
-    console.error(e);
+
+try{
+
+  //ON page change
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if(changeInfo.status == 'complete'){
+    //if (changeInfo.url) {
+      chrome.scripting.executeScript({
+        files: ['contentScript.js'],
+        target: {tabId: tab.id}
+      });
+    //}
+    }
+  });
+
+
+}catch(e){
+  console.log(e);
 }
--- extensions/scripts/test.js
+++
@@ -1,9 +0,0 @@
-if(typeof init !== 'function') {
-    const init = function() {
-        const injectElement = document.createElement('div');
-        injectElement.id = 'injectElement';
-        injectElement.innerHTML = 'Injected Element';
-        document.body.appendChild(injectElement);
-    }
-    init();
-}
