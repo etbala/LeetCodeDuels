0357bcdae65b4d2823a1008d77db4341891c97f4
EdenBendheim <edenbendheim@gmail.com>
2024-04-24 19:59:26 -0400

    working getting info from page

--- extension/scripts/contentScript.js
+++ extension/scripts/contentScript.js
@@ -2,7 +2,7 @@
 (function() {
   // Create the new div element
   const newDiv = document.createElement('div');
-  newDiv.innerHTML = "<strong>Opponent: 76% runtime | 33% Memory</strong>";
+  newDiv.innerHTML = "<strong>No stats yet</strong>";
   newDiv.style.position = 'fixed';
   newDiv.style.bottom = '20px';
   newDiv.style.right = '20px';
@@ -10,25 +10,33 @@
   newDiv.style.padding = '10px';
   newDiv.style.borderRadius = '5px';
   newDiv.style.boxShadow = '0 2px 4px rgba(0,0,0,0.2)';
+  newDiv.id = 'current-stats';
 
   // Append the new div to the body of the document
   document.body.appendChild(newDiv);
   console.log('Content script loaded');
-  
-  const targetButtons = document.getElementById('ide-top-btns');
-  // make the buttons blue
-  targetButtons.style.display = 'none';
-  for (let i = 0; i < 3; i++) {
-    console.log('Button found');
-    // targetButtons[i].addEventListener("click", function() {
-    //   // This function will be executed when the button is clicked
-    //   // You can add your desired actions here, like sending a message to the background script
-    //   chrome.runtime.sendMessage({ message: "buttonClicked" });
-    //   console.log('Button clicked');
-    // });
-  }
-  
-  
+  setTimeout(function() {
+    const targetButtons = document.querySelectorAll('[data-e2e-locator="console-submit-button"]');
+    console.log(targetButtons);
+    // make the button console.log the text of the button on click
+    targetButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        console.log('Button clicked');
+        // wait 5 seconds, then find spans with this class `class="text-sd-foreground text-lg font-semibold"`
+        setTimeout(function() {
+          const stats = document.getElementsByClassName('text-sd-foreground text-lg font-semibold');
+          console.log(stats);
+          const statsArray = Array.from(stats);
+          const statsText = statsArray.map(stat => stat.innerText);
+          console.log(statsText);
+          // Update the div with the new stats
+          const currentStatsDiv = document.getElementById('current-stats');
+          currentStatsDiv.innerHTML = statsText.join('<br>');
+        }, 7000);
+      });
+    });
+  }, 5000);
+
 })();
 
 
