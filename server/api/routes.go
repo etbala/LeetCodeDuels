6575e3d30ad78cdba81067fc699d21edadaffa0f
Ethan Balakumar <76022348+etbala@users.noreply.github.com>
2025-05-19 02:12:59 -0500

    Add Routes Plan

--- server/api/routes.go
+++ server/api/routes.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"leetcodeduels/database"
-	"leetcodeduels/handlers"
 
 	"github.com/gorilla/mux"
 )
@@ -13,51 +12,105 @@ import (
 func SetupRoutes(authMiddleware mux.MiddlewareFunc, db database.I_DB) *mux.Router {
 	r := mux.NewRouter()
 
-	// --------------------
+	// ----------------------
 	// Authentication Routes
-	// --------------------
-	publicAuthRouter := r.PathPrefix("/auth").Subrouter()
-	protectedAuthRouter := r.PathPrefix("/auth").Subrouter()
-	protectedAuthRouter.Use(authMiddleware)
+	// ----------------------
+	authRouter := r.PathPrefix("/auth").Subrouter()
 
-	// Public routes for initiating and handling GitHub OAuth
-	publicAuthRouter.HandleFunc("/github/initiate", func(w http.ResponseWriter, r *http.Request) {
-		handlers.AuthGitHubInitiate(w, r)
+	authRouter.HandleFunc("/github/initiate", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.AuthGitHubInitiate(w, r)
 	}).Methods("GET")
 
-	publicAuthRouter.HandleFunc("/github/callback", func(w http.ResponseWriter, r *http.Request) {
-		handlers.AuthGitHubCallback(w, r)
+	authRouter.HandleFunc("/github/callback", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.AuthGitHubCallback(w, r)
 	}).Methods("GET")
 
-	// Authenticated endpoint to get current user info
-	protectedAuthRouter.HandleFunc("/me", func(w http.ResponseWriter, r *http.Request) {
-		// handlers.AuthMe(w, r)
+	// ----------------------
+	// User / Account Routes
+	// ----------------------
+	accountRouter := r.PathPrefix("/user").Subrouter()
+	accountRouter.Use(authMiddleware)
+
+	// TODO: Friend System (friend invites & notification for invites system)
+
+	accountRouter.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.GetProfile(w, r)
 	}).Methods("GET")
 
-	// --------------------
-	// Match & Game Session Routes
-	// --------------------
-	// All matches-related routes require authentication
-	publicMatchRouter := r.PathPrefix("/matches").Subrouter()
-	protectedMatchRouter := r.PathPrefix("/matches").Subrouter()
-	protectedMatchRouter.Use(authMiddleware)
+	// Get current user's profile information
+	accountRouter.HandleFunc("/me", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.MyProfile(w, r)
+	}).Methods("GET")
 
-	// Invite and accept match
-	protectedMatchRouter.HandleFunc("/invite", func(w http.ResponseWriter, r *http.Request) {
-		// handlers.MatchesInvite(w, r)
+	accountRouter.HandleFunc("/me/onboard", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.CreateUser(w, r)
+	}).Methods("GET")
+
+	accountRouter.HandleFunc("/me/delete", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.DeleteUser(w, r)
+	}).Methods("GET")
+
+	// Change username
+	accountRouter.HandleFunc("/me/rename", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.RenameUser(w, r)
+	}).Methods("GET")
+
+	// Change associated leetcode username
+	accountRouter.HandleFunc("/me/lcrename", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.RenameLCUser(w, r)
+	}).Methods("GET")
+
+	// ----------------------
+	// Invitation Routes
+	// ----------------------
+	inviteRouter := r.PathPrefix("/invites").Subrouter()
+	inviteRouter.Use(authMiddleware)
+
+	inviteRouter.HandleFunc("/invite", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.SendInvite(w, r)
 	}).Methods("POST")
 
-	protectedMatchRouter.HandleFunc("/accept", func(w http.ResponseWriter, r *http.Request) {
-		// handlers.MatchesAccept(w, r)
+	inviteRouter.HandleFunc("/invite/cancel", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.CancelInvite(w, r)
 	}).Methods("POST")
+
+	inviteRouter.HandleFunc("/invite/{id}/accept", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.AcceptInvite(w, r)
+	}).Methods("POST")
+
+	inviteRouter.HandleFunc("/invite/{id}/decline", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.DeclineInvite(w, r)
+	}).Methods("POST")
+
+	// ----------------------
+	// Matchmaking Routes
+	// ----------------------
+	mmRouter := r.PathPrefix("/matchmake").Subrouter()
+	mmRouter.Use(authMiddleware)
+
+	// TODO: Figure out nice way to pass all params through /enter
+
+	mmRouter.HandleFunc("/enter", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.EnterQueue(w, r)
+	}).Methods("POST")
+
+	mmRouter.HandleFunc("/leave", func(w http.ResponseWriter, r *http.Request) {
+		// handlers.LeaveQueue(w, r)
+	}).Methods("POST")
+
+	// ----------------------
+	// Game Session Routes
+	// ----------------------
+	matchRouter := r.PathPrefix("/game").Subrouter()
+	matchRouter.Use(authMiddleware)
 
 	// Get match details
-	publicMatchRouter.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) {
+	matchRouter.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) {
 		// handlers.MatchesGet(w, r)
 	}).Methods("GET")
 
-	// Submit code for a match
-	protectedMatchRouter.HandleFunc("/{id}/submission", func(w http.ResponseWriter, r *http.Request) {
+	// Send code submission results/details
+	matchRouter.HandleFunc("/{id}/submission", func(w http.ResponseWriter, r *http.Request) {
 		// handlers.MatchesSubmit(w, r)
 	}).Methods("POST")
 
