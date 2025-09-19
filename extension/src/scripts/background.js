cc6df80459927d97c3b37d2b18e1b88e4cd98906
Ethan <76022348+etbala@users.noreply.github.com>
2025-09-19 16:05:43 -0400

    Update server url

--- extension/src/scripts/background.js
+++ extension/src/scripts/background.js
@@ -14,7 +14,8 @@ try {
     console.log(e);
 }
 
-const SERVER_URL = 'http://localhost:8080';
+// const SERVER_URL = 'http://localhost:8080';
+const SERVER_URL = 'https://lc-duels-development.fly.dev/';
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'getServerUrl') {
         sendResponse({ serverUrl: SERVER_URL });
