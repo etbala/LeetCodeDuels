9cfe4e55e37b48e047b806575ddc98dcf395a87c
Ethan Balakumar <76022348+etbala@users.noreply.github.com>
2025-09-27 17:30:54 -0400

    Add logger

---
+++ server/logging/logging.go
@@ -0,0 +1,69 @@
+package logging
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/rs/zerolog"
+)
+
+var Logger zerolog.Logger
+
+func init() {
+	Logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
+}
+
+// responseWriter is a minimal wrapper for http.ResponseWriter that allows the
+// written HTTP status code to be captured for logging.
+type responseWriter struct {
+	http.ResponseWriter
+	status      int
+	wroteHeader bool
+}
+
+func wrapResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{ResponseWriter: w}
+}
+
+func (rw *responseWriter) Status() int {
+	return rw.status
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	if rw.wroteHeader {
+		return
+	}
+	rw.status = code
+	rw.ResponseWriter.WriteHeader(code)
+	rw.wroteHeader = true
+}
+
+func RequestLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		requestID := uuid.New().String()
+
+		l := Logger.With().
+			Str("request_id", requestID).
+			Str("method", r.Method).
+			Str("path", r.URL.Path).
+			Str("remote_addr", r.RemoteAddr).
+			Str("user_agent", r.UserAgent()).
+			Logger()
+
+		ctx := l.WithContext(r.Context())
+
+		w.Header().Set("X-Request-ID", requestID)
+
+		wrapped := wrapResponseWriter(w)
+		next.ServeHTTP(wrapped, r.WithContext(ctx))
+
+		l.Info().
+			Int("status_code", wrapped.Status()).
+			Dur("duration_ms", time.Since(start)).
+			Msg("Request completed")
+	})
+}
