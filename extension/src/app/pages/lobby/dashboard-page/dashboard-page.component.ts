9650282b941ccbb0c80e75135923793218209087
Ethan <76022348+etbala@users.noreply.github.com>
2025-10-10 16:26:26 -0400

    Fix environment being used

--- extension/src/app/pages/lobby/dashboard-page/dashboard-page.component.ts
+++ extension/src/app/pages/lobby/dashboard-page/dashboard-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../../environments/environment.prod';
+import { environment } from '../../../../environments/environment';
 import { Router } from '@angular/router';
 import { Tag } from '../../../models/tag';
 
--- extension/src/app/services/auth/auth.service.ts
+++ extension/src/app/services/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
-import { environment } from '../../../environments/environment.prod';
+import { environment } from '../../../environments/environment';
 
 interface User {
   id: string;
