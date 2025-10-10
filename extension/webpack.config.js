2972d944e84913b55965d06ffa8c342884cca715
Ethan <76022348+etbala@users.noreply.github.com>
2025-10-10 16:49:06 -0400

    Compile ts scripts as standalone

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -4,5 +4,7 @@ module.exports = (config, options) => {
   config.entry.background = path.join(__dirname, 'src/scripts/background.ts');
   config.entry.content = path.join(__dirname, 'src/scripts/content.ts');
 
+  config.optimization = config.optimization || {};
+  config.optimization.runtimeChunk = false;
   return config;
 };
