9c05d5946e39c132240e24098618330ef81ed0fe
Ethan Balakumar <76022348+etbala@users.noreply.github.com>
2025-11-13 00:06:17 -0500

    Use existing configs for firefox builds

--- extension/build-firefox.js
+++ extension/build-firefox.js
@@ -1,11 +1,25 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
 
+let config = 'dev';
+
+if (process.env.npm_config_config) {
+  // This is populated by running `npm run ... --config=local`
+  config = process.env.npm_config_config;
+  console.log(`Found config from --config argument: ${config}`);
+} else if (process.env.npm_config_configuration) {
+  // This is populated by running `npm run ... --configuration=local`
+  config = process.env.npm_config_configuration;
+  console.log(`Found config from --configuration argument: ${config}`);
+} else {
+  console.log(`No --config or --configuration argument found in env. Defaulting to "${config}".`);
+}
+
 fs.copyFileSync('src/manifest.json', 'src/manifest.json.backup');
 fs.copyFileSync('src/manifest.firefox.json', 'src/manifest.json');
 
 try {
-  execSync('ng build --configuration=firefox', { stdio: 'inherit' });
+  execSync(`ng build --configuration=${config}`, { stdio: 'inherit' });
 } finally {
   fs.copyFileSync('src/manifest.json.backup', 'src/manifest.json');
   fs.unlinkSync('src/manifest.json.backup');
