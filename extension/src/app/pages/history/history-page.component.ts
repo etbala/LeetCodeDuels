795274a23d23d529d8c9533f9d9ff473e12cd28b
Tre'Vaughn Barboza <trerese3@gmail.com>
2025-11-14 14:35:37 -0500

    Unified loading messages

--- extension/src/app/pages/history/history-page.component.ts
+++ extension/src/app/pages/history/history-page.component.ts
@@ -6,7 +6,7 @@ import { UserService } from 'services/api/user.service';
 import { User } from 'models/user.model';
 import { Session } from 'models/match';
 
-import { switchMap, map } from 'rxjs/operators';
+import { switchMap, map, finalize } from 'rxjs/operators';
 import { forkJoin, of } from 'rxjs';
 
 type EnrichedSession = Session & { opponent?: User };
@@ -22,12 +22,15 @@ export class HistoryPageComponent implements OnInit {
   userId: number | null = null;
   sessions: EnrichedSession[] = [];
   errorText: string | null = null;
+  isLoading = false;
 
   constructor(
     private userService: UserService
   ) {}
 
   ngOnInit(): void {
+    this.isLoading = true;
+
     this.userService
       .getMyProfile()
       .pipe(
@@ -35,7 +38,8 @@ export class HistoryPageComponent implements OnInit {
           this.userId = id;
           return this.getSessions(id);
         }),
-        switchMap((sessions) => this.addOpponents(sessions))
+        switchMap((sessions) => this.addOpponents(sessions)),
+        finalize(() => (this.isLoading = false))
       )
       .subscribe({
         next: (data) => (this.sessions = data),
